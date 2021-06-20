aae2b9b74391f0b4bd24dbb5265630167268f985
Khalil Al-Mujeeb <khalilalm@users.noreply.github.com>
2021-06-20 12:31:14 -0400

    Adding paypal donation component

--- src/components/HomeComponents/HomeDonationsComponent.js
+++ src/components/HomeComponents/HomeDonationsComponent.js
@@ -39,7 +39,14 @@ function HomeDonations() {
                             <div class="col-details">
                                 <p>Lorem ipsum dolor sit amet consect adipisscin elit proin vel lectus ut eta esami vera dolor sit amet consect</p>
                             </div>
-                            <a href="#" class="btn btn-primary"> Donate </a>
+                            <form action="https://www.paypal.com/donate" method="post" target="_top">
+<input type="hidden" name="business" value="nesstex@gmail.com" />
+<input type="hidden" name="no_recurring" value="0" />
+<input type="hidden" name="item_name" value="charity" />
+<input type="hidden" name="currency_code" value="USD" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+</form>
                         </div>
                     </div>
                     {/* <div class="col-md-3 col-sm-6">
